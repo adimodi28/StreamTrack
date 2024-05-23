a14aad987918c745b1ea72970c78df5c9b834896
adimodi28 <smiami280@gmail.com>
2024-05-22 22:58:03 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-# No need for apt-get commands as the filesystem is read-only
 # Ensure using pre-built wheels for Python packages
-pip install -r requirements.txt
+pip install --no-cache-dir --only-binary :all: -r requirements.txt
