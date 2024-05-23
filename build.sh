2f68987d3402c92681db36e6828cb60d06036e8c
adimodi28 <smiami280@gmail.com>
2024-05-22 22:42:34 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Install system dependencies
-apt-get update && apt-get install -y build-essential
+apt-get update && apt-get install -y build-essential gfortran libopenblas-dev liblapack-dev
